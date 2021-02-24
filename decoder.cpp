f399b0f12dcbe2d98bb9d306d5a5a665a06034e8
Petrucio Neves <petrucio-filho@hotmail.com>
2021-02-24 00:52:24 -0300

    commit

--- decoder.cpp
+++ decoder.cpp
@@ -331,5 +331,180 @@ void opcode(char *hex){
                 ", r" << convertebindec(separabinario(binario,10,3), 3)<<endl;
             }
         }
+        //CPY
+        else if(hex[1]=='6' && binario[8]=='0' && binario[9]=='0'){
+            fout << hex << "    CPY r" << convertebindec(separabinario(binario,13,3), 3) <<
+                ", r" << convertebindec(separabinario(binario,10,3), 3)<<endl;
+        }
+        //ADD Ld,Hm
+        else if(hex[1]=='4' && binario[8]=='0' && binario[9]=='1'){
+            fout << hex << "    ADD r" << convertebindec(separabinario(binario,13,3), 3) <<
+                ", r" << convertebindec(separabinario(binario,10,3), 3)<<endl;
+        }
+        //MOV Ld,Hm
+        else if(hex[1]=='6' && binario[8]=='0' && binario[9]=='1'){
+            fout << hex << "    MOV r" << convertebindec(separabinario(binario,13,3), 3) <<
+                ", r" << convertebindec(separabinario(binario,10,3), 3)<<endl;
+        }
+        //ADD Hd,Lm
+        else if(hex[1]=='4' && binario[8]=='1' && binario[9]=='0'){
+            fout << hex << "    ADD r" << convertebindec(separabinario(binario,13,3), 3) <<
+                ", r" << convertebindec(separabinario(binario,10,3), 3)<<endl;
+        }
+        //MOV Hd,Lm
+        else if(hex[1]=='6' && binario[8]=='1' && binario[9]=='0'){
+            fout << hex << "    MOV r" << convertebindec(separabinario(binario,13,3), 3) <<
+                ", r" << convertebindec(separabinario(binario,10,3), 3)<<endl;
+        }
+        //ADD Hd,Hm
+        else if(hex[1]=='4' && binario[8]=='1' && binario[9]=='1'){
+            fout << hex << "    ADD r" << convertebindec(separabinario(binario,13,3), 3) <<
+                ", r" << convertebindec(separabinario(binario,10,3), 3)<<endl;
+        }
+        //MOV Hd,Hm
+        else if(hex[1]=='6' && binario[8]=='1' && binario[9]=='1'){
+            fout << hex << "    MOV r" << convertebindec(separabinario(binario,13,3), 3) <<
+                ", r" << convertebindec(separabinario(binario,10,3), 3)<<endl;
+        }
+        //CMP
+        else if(hex[1]=='5' && binario[8]=='0' && binario[9]=='1'){
+            fout << hex << "    CMP r" << convertebindec(separabinario(binario,13,3), 3) <<
+                ", r" << convertebindec(separabinario(binario,10,3), 3)<<endl;
+        }
+        //CMP
+        else if(hex[1]=='5' && binario[8]=='1' && binario[9]=='0'){
+            fout << hex << "    CMP r" << convertebindec(separabinario(binario,13,3), 3) <<
+                ", r" << convertebindec(separabinario(binario,10,3), 3)<<endl;
+        }
+        //CMP
+        else if(hex[1]=='5' && binario[8]=='1' && binario[9]=='1'){
+            fout << hex << "    CMP r" << convertebindec(separabinario(binario,13,3), 3) <<
+                ", r" << convertebindec(separabinario(binario,10,3), 3)<<endl;
+        }
+        //BX | BLX
+        else if(hex[1]=='7' && binario[13]=='0' && binario[14]=='0' && binario[15]=='0'){
+            //BX
+            if(binario[8]=='0'){
+                fout << hex << "    BX r" << convertebindec(separabinario(binario,9,4), 4)<<endl;
+            }//BLX
+            else{
+                fout << hex << "    BLX r" << convertebindec(separabinario(binario,9,4), 4)<<endl;
+            }
+        }
+        //LDR
+        else if(binario[4]=='1'){
+            fout << hex << "    LDR r" << convertebindec(separabinario(binario,5,3), 3)<<
+            ", [pc, #" << convertebindec(separabinario(binario,8,8), 8) << "]" <<endl;
+        }
+    }
+    //STR | STRH | STRB | LDRSB
+    else if(hex[0]=='5'){
+        if(binario[4]=='0'){
+            //STR
+            if(binario[5]=='0' && binario[6]=='0'){
+                fout << hex << "    STR r" << convertebindec(separabinario(binario,13,3), 3)<<
+                                ", r" << convertebindec(separabinario(binario,10,3), 3) << ", r" <<
+                                convertebindec(separabinario(binario,7,3), 3) << endl;
+            }
+            //STRH
+            else if(binario[5]=='0' && binario[6]=='1'){
+                fout << hex << "    STRH r" << convertebindec(separabinario(binario,13,3), 3)<<
+                                ", r" << convertebindec(separabinario(binario,10,3), 3) << ", r" <<
+                                convertebindec(separabinario(binario,7,3), 3) << endl;
+            }
+            //STRB
+            else if(binario[5]=='1' && binario[6]=='0'){
+                fout << hex << "    STRB r" << convertebindec(separabinario(binario,13,3), 3)<<
+                                ", r" << convertebindec(separabinario(binario,10,3), 3) << ", r" <<
+                                convertebindec(separabinario(binario,7,3), 3) << endl;
+            }
+            //LDRSB
+            else if(binario[5]=='1' && binario[6]=='1'){
+                fout << hex << "    LDRSB r" << convertebindec(separabinario(binario,13,3), 3)<<
+                                ", r" << convertebindec(separabinario(binario,10,3), 3) << ", r" <<
+                                convertebindec(separabinario(binario,7,3), 3) << endl;
+            }
+        }
+        //LDR | LDRH | LDRB | LDRSH
+        else if(binario[4]=='1'){
+            //LDR
+            if(binario[5]=='0' && binario[6]=='0'){
+                fout << hex << "    LDR r" << convertebindec(separabinario(binario,13,3), 3)<<
+                                ", r" << convertebindec(separabinario(binario,10,3), 3) << ", r" <<
+                                convertebindec(separabinario(binario,7,3), 3) << endl;
+            }
+            //LDRH
+            else if(binario[5]=='0' && binario[6]=='1'){
+                fout << hex << "    LDRH r" << convertebindec(separabinario(binario,13,3), 3)<<
+                                ", r" << convertebindec(separabinario(binario,10,3), 3) << ", r" <<
+                                convertebindec(separabinario(binario,7,3), 3) << endl;
+            }
+            //LDRB
+            else if(binario[5]=='1' && binario[6]=='0'){
+                fout << hex << "    LDRB r" << convertebindec(separabinario(binario,13,3), 3)<<
+                                ", r" << convertebindec(separabinario(binario,10,3), 3) << ", r" <<
+                                convertebindec(separabinario(binario,7,3), 3) << endl;
+            }
+            //LDRSH
+            else if(binario[5]=='1' && binario[6]=='1'){
+                fout << hex << "    LDRSH r" << convertebindec(separabinario(binario,13,3), 3)<<
+                                ", r" << convertebindec(separabinario(binario,10,3), 3) << ", r" <<
+                                convertebindec(separabinario(binario,7,3), 3) << endl;
+            }
+        }
+    }
+    //STR | LDR Ld, [Ln, #immed*4]
+    else if(hex[0]=='6'){
+        //STR Ld, [Ln, #immed*4]
+        if(binario[4]=='0'){
+            fout << hex << "    STR r" << convertebindec(separabinario(binario,13,3), 3)<<
+            ", [r"<< convertebindec(separabinario(binario,10,3), 3)<<", #" << 
+            convertebindec(separabinario(binario,5,5), 5) << "]" <<endl;
+        }//LDR Ld, [Ln, #immed*4]
+        else{
+            fout << hex << "    LDR r" << convertebindec(separabinario(binario,13,3), 3)<<
+            ", [r"<< convertebindec(separabinario(binario,10,3), 3)<<", #" << 
+            convertebindec(separabinario(binario,5,5), 5) << "]" <<endl;
+        }
+    }
+    //STRB | LDRB Ld, [Ln, #immed]
+    else if(hex[0]=='7'){
+        //STRB Ld, [Ln, #immed*4]
+        if(binario[4]=='0'){
+            fout << hex << "    STRB r" << convertebindec(separabinario(binario,13,3), 3)<<
+            ", [r"<< convertebindec(separabinario(binario,10,3), 3)<<", #" << 
+            convertebindec(separabinario(binario,5,5), 5) << "]" <<endl;
+        }//LDRB Ld, [Ln, #immed*4]
+        else{
+            fout << hex << "    LDRB r" << convertebindec(separabinario(binario,13,3), 3)<<
+            ", [r"<< convertebindec(separabinario(binario,10,3), 3)<<", #" << 
+            convertebindec(separabinario(binario,5,5), 5) << "]" <<endl;
+        }
+    }
+    //STRH | LDRH Ld, [Ln, #immed]
+    else if(hex[0]=='8'){
+        //STRH Ld, [Ln, #immed*4]
+        if(binario[4]=='0'){
+            fout << hex << "    STRH r" << convertebindec(separabinario(binario,13,3), 3)<<
+            ", [r"<< convertebindec(separabinario(binario,10,3), 3)<<", #" << 
+            convertebindec(separabinario(binario,5,5), 5) << "]" <<endl;
+        }//LDRH Ld, [Ln, #immed*4]
+        else{
+            fout << hex << "    LDRH r" << convertebindec(separabinario(binario,13,3), 3)<<
+            ", [r"<< convertebindec(separabinario(binario,10,3), 3)<<", #" << 
+            convertebindec(separabinario(binario,5,5), 5) << "]" <<endl;
+        }
+    }
+    //STR | LDR Ld, [sp, #immed*4]
+    else if(hex[0]=='8'){
+        //STR Ld, [sp, #immed*4]
+        if(binario[4]=='0'){
+            fout << hex << "    STR r" << convertebindec(separabinario(binario,5,3), 3)<<
+            ", [sp, #" << convertebindec(separabinario(binario,8,8), 8) << "]" <<endl;
+        }//LDR Ld, [sp, #immed*4]
+        else{
+            fout << hex << "    LDR r" << convertebindec(separabinario(binario,5,3), 3)<<
+            ", [sp, #" << convertebindec(separabinario(binario,8,8), 8) << "]" <<endl;
+        }
     }
 }
